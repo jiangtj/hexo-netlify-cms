75b0bf0ee6f5edad0aa581dd01ac987bdc0b1b16
MrTT <jiang.taojie@foxmail.com>
2019-01-06 21:48:18 +0800

    v0.0.4

--- index.js
+++ index.js
@@ -1,16 +1,17 @@
-var fs = require('hexo-fs');
+var fs = require('fs');
+var path = require('path');
 var yaml = require('js-yaml');
 
-hexo.extend.generator.register('asset', function(locals){
+hexo.extend.generator.register('generateNetlifyCMS', function(locals){
   return [{
     path: 'admin/index.html',
-    data:  function(){
-        fs.readFileSync('admin/index.html')
+    data: function(){
+      return fs.readFileSync(path.join(__dirname, 'admin/index.html'));
     }
   },{
     path: 'admin/config.yml',
     data: function(){
-      var config = yaml.safeLoad(fs.readFileSync('admin/config.yml'));
+      var config = yaml.safeLoad(fs.readFileSync(path.join(__dirname, 'admin/config.yml')));
       Object.assign(config, hexo.theme.config.netlify_cms, hexo.config.netlify_cms)
       return yaml.safeDump(config);
     }
