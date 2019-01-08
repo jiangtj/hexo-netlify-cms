05769076f2cc69dd3704bbc1321b1cdbc26205d9
MrTT <jiang.taojie@foxmail.com>
2019-01-08 23:04:23 +0800

    Add custom tag support

--- index.js
+++ index.js
@@ -3,61 +3,27 @@
 const fs = require('fs');
 const path = require('path');
 const yaml = require('js-yaml');
-const ejs = require('ejs');
+const renderAdmin = require('./lib/renderAdmin');
+const cmsGenerator = require('./lib/cmsGenerator');
 
 hexo.extend.generator.register('generateNetlifyCMS', function(locals){
+
+  //Get config
+  let hexoConfig = Object.assign({}, hexo.theme.config.netlify_cms, hexo.config.netlify_cms);
+  let filePath = hexoConfig.config_file||path.join(__dirname, 'admin/config.yml');
+  let yamlConfig = yaml.safeLoad(fs.readFileSync(filePath));
+  Object.assign(yamlConfig,hexoConfig)
+
   return [{
     path: 'admin/index.html',
     data: function(){
-      if (!hexo.config.netlify_cms){
-        hexo.config.netlify_cms= {};
-      }
-      if (!hexo.config.netlify_cms.scripts){
-        hexo.config.netlify_cms.scripts = [];
-      }
-      return hexo.render.renderSync({path:path.join(__dirname, 'admin/index.ejs')},hexo.config);
+      return renderAdmin(hexo,yamlConfig);
     }
   },{
     path: 'admin/config.yml',
     data: function(){
-      //Get config
-      let hexoConfig = Object.assign({}, hexo.theme.config.netlify_cms, hexo.config.netlify_cms);
-      let filePath = hexoConfig.config_file||path.join(__dirname, 'admin/config.yml');
-      let yamlConfig = yaml.safeLoad(fs.readFileSync(filePath));
-      let config = Object.assign(yamlConfig,hexoConfig)
-      //Auto pages
-      if (config.pages.enabled) {
-        let pageConfig = config.pages.config;
-        //Get fields
-        let pageFields = pageConfig.fields;
-        if (!pageFields) {
-          config.collections.forEach(element => {
-            if (element.name === 'posts') {
-              pageFields = element.fields;
-            }
-          });
-        }
-        //Get files from locals var
-        let pageFiles = locals.pages.data.map(function(page) {
-          return {
-            name: page.source.replace(/\//g, '-'),
-            label: page.title,
-            file: path.join(hexo.config.source_dir,page.source),
-            editor:{
-              preview: pageConfig.editor.preview
-            },
-            fields: pageFields
-          }
-        });
-        //Add pages collection
-        yamlConfig.collections.push({
-          label: pageConfig.label,
-          name: 'pages',
-          delete: pageConfig.delete,
-          files: pageFiles
-        });
-      }
-      return yaml.safeDump(yamlConfig);
+      return cmsGenerator(hexo,locals,yamlConfig);
     }
   }];
+  
 });
