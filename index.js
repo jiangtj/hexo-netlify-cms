e43595b33225432e176ebab68a8eecc3c4de0094
jiangtj <116749895@qq.com>
2019-08-08 18:15:17 +0800

    Fix fields

--- index.js
+++ index.js
@@ -7,31 +7,42 @@ const merge = require('lodash/merge');
 const renderAdmin = require('./lib/renderAdmin');
 const cmsGenerator = require('./lib/cmsGenerator');
 
-hexo.extend.generator.register('generateNetlifyCMS', function(locals){
+hexo.extend.generator.register('generateNetlifyCMS', (locals) => {
 
-  let hexoConfig = merge({}, hexo.theme.config.netlify_cms, hexo.config.netlify_cms);
+  let hexoConfig = hexo.config.netlify_cms || {};
   let defaultConfig = yaml.safeLoad(fs.readFileSync(path.join(__dirname, 'admin/config.yml')));
   let configFileConfig = {}
   if (hexoConfig.config_file) {
     configFileConfig = yaml.safeLoad(fs.readFileSync(hexoConfig.config_file));
-    delete(hexoConfig.config_file);
+    delete (hexoConfig.config_file);
   }
 
-  let config = merge(defaultConfig, configFileConfig, hexoConfig);
+
+  let fields = Object.assign({},
+    hexoConfig.global_fields,
+    configFileConfig.global_fields,
+    defaultConfig.global_fields
+  );
+
+  delete (hexoConfig.global_fields);
+  delete (configFileConfig.global_fields);
+  delete (defaultConfig.global_fields);
+
+  let config = merge({}, defaultConfig, configFileConfig, hexoConfig);
 
   let scripts = config.scripts;
-  delete(config.scripts);
+  delete (config.scripts);
 
   return [{
     path: 'admin/index.html',
-    data: function(){
+    data: function () {
       return renderAdmin(hexo, scripts);
     }
-  },{
+  }, {
     path: 'admin/config.yml',
-    data: function(){
-      return cmsGenerator(hexo,locals,config);
+    data: function () {
+      return cmsGenerator(hexo, locals, config, fields);
     }
   }];
-  
+
 });
